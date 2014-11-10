4f5b7cbe44a53bddc5e392db0ea37c8bef6b8d9a
jw149j <Jonathan.Wilkes@glasgow.ac.uk>
2014-11-10 14:52:33 +0000

    course project due 23Nov - stage002

--- run_analysis.R
+++ run_analysis.R
@@ -4,28 +4,35 @@ merge_and_label<-function(){
 # get listing of integer/position - feature label  mappings
   featureLabels<-read.table("features.txt")
 # get the training set:
-  trainSet<-do_rbind("train")
+  trainSet<-do_cbind("train")
   print(trainSet)
 # get the test set  
-  testSet<-do_rbind("test")
-print(testSet)
+  testSet<-do_cbind("test")
+  if((trainSet==-1) || (testSet ==-1)) return("the contents are NOT matched by number of rows")
 # 
 }
 
-do_rbind<-function(d){
-  # activity type record:
+do_cbind<-function(d){
+# activity type record file name
   Act<-paste(d,"/y_",d,".txt",sep="")
+# subject id record file name  
   subjID<-paste(d,"/subject_",d,".txt",sep="")
+# feature vector filename
   vectors<-paste(d,"/X_",d,".txt",sep="")
   print(Act)
   print(subjID)
   print(vectors)
   Act<-read.table(Act)
+  Act<-sapply(Act,as.numeric)
   # subjectID
   subjID<-read.table(subjID)
+  subjID<-sapply(subjID,as.numeric)
   # feature vector values
-  vectors<-read.table(vectors) 
+  vectors<-read.table(vectors)
+  vectors<-sapply(vectors,as.numeric)
+print(class(Act))
+print(class(subjID))
+print(class(vectors))
   if((nrow(vectors)!=nrow(subjID)) || (nrow(vectors)!=nrow(Act))){return(-1)}
-  1
-  
+  cbind(subjID,Act,vectors)
 }
