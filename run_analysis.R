79cfd301f6c08e4f10549efe8b11555f83d1b757
jw149j <Jonathan.Wilkes@glasgow.ac.uk>
2014-11-11 15:49:11 +0000

    Final submission form ??

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,22 @@
-##\#############################[ merge_subset_and_label() ]##########################################
+##\############################################[ run_analysis() ]#############################################
+##\ Function to perform the data cleaning and summarizing specified in the "Getting and Cleaning Data"
+##\ course as listed in the programming project specifications.
+##\ It will work as specified if it is run while the setwd() specifies the Global Environment as
+##\ the top level directory of the UCIHAR dataset
+##\ The initial merging, cleaning and selection of the data and assignment of meaningful labels
+##\ is perfomed by the function merge_subset_and_label()
+##\ This dataset (in the form of a data frame) is subsequently summarized by the function summary_mean(df)
+##\ by aggregating subsets of data corresponding to individual subject/activity labels, taking the 
+##\ average of the mean values for each subset. This data is written to a text file named "data_mean_summ.txt"
+##\###########################################################################################################
+run_analysis<-function(){
+   cleanup<-merge_subset_and_label()
+   summary_mean(cleanup)
+}
+
+
+
+##\####################################[ merge_subset_and_label() ]##########################################
 ##\Function to :
 ##\==(1) read the files summarising the numerical/positional codes and explicit labels for the 
 ##\   features recorded in these datasets 
@@ -12,7 +30,7 @@
 ##\==(6) give the columns the explicit labels as column names, convert to a data frames and convert 
 ##\   the numeric activity factors into explicit labels
 ##\==(7) return the data frame
-##\#############################################################################################
+##\#########################################################################################################
 merge_subset_and_label<-function(){
 ##\==(1)  
 #\  get listing of integer - activity label mappings
@@ -47,7 +65,7 @@ merge_subset_and_label<-function(){
 ##\==(6)
 #\ subset the full feature table to only the required features 
     reqTable<-totalSet[,requiredFeats]
-    print(dim(reqTable))
+    #print(dim(reqTable))
 #\ attach the vector of required feature labels as column names
     colnames(reqTable)<-featureLabels[requiredFeats]
 #\ convert the matrix to a data.frame
@@ -55,14 +73,14 @@ merge_subset_and_label<-function(){
 #\ assign the explicit activity strings to the numerical factors maintained in the activities column
     reqTable$Activity<-ordered(reqTable$Activity,  levels = activities[,1],  labels = activities[,2])
 #\ take a peek (comment out after development)
-   print(head(reqTable,50))
+   #print(head(reqTable,50))
 ##\==(7)
 #\ return the filtered, parsed and relabelled dataframe
    reqTable
 }
 
 
-##\################################[ do_cbind<-function(d) ]#####################################
+##\################################################[ do_cbind<-function(d) ]#####################################
 ##\ Function to :
 ##\ ==(1) accept a token ("test" or "train") to enable the explicit generation of valid
 ##\    filenames for i) the codes for the activity associated with each observation ii) the subject
@@ -73,15 +91,17 @@ merge_subset_and_label<-function(){
 ##\ ==(3) If equal,convert all data structures to matrices and prepend the subjectIDs and activity
 ##\    codes to the left hand side (columns 1 and 2) of the matrix which.
 ##\ ==(4) return the constructed matrix
-##\##############################################################################################
+##\##############################################################################################################
 
 do_cbind<-function(d){
+##\ ==(1)  
 #\ construct the activity type record file name
   Act<-paste(d,"/y_",d,".txt",sep="")
 #\ construct thesubject id record file name  
   subjID<-paste(d,"/subject_",d,".txt",sep="")
 #\ construct the feature vector filename
   vectors<-paste(d,"/X_",d,".txt",sep="")
+##\ ==(2)
 #\  read activity ids file  into a data frame
    Act<-read.table(Act)
 #\  extract the activity ids as a numeric vector 
@@ -96,13 +116,15 @@ do_cbind<-function(d){
   vectors<-sapply(vectors,as.numeric)
 #\ check the number of observations equal in all three objects
    if((nrow(vectors)!=nrow(subjID)) || (nrow(vectors)!=nrow(Act))){return(-1)}
+##\ ==(3)
 #\ if passes check, column bind the subjectID and activity codes to the feature values
    ret<-cbind(subjID,Act,vectors)
 #\ return assembled matrix
-   ret
+##\ ==(4)
+ret
 }
 
-##\############################[ summary_mean(df) ]##################################
+##\############################[ summary_mean(df) ]##############################################################
 ##\ Function to:
 ##\ ==(1)take a tidied data set in data frame format
 ##\ ==(2) discard all columns where the label indicates the data consists of standard 
@@ -111,18 +133,21 @@ do_cbind<-function(d){
 ##\   first two columns and calculates the mean value of the subset 
 ##\ ==(4) edit the column labels for the two factors to meaningful values 
 ##\ ==(5) Output the summary to a specified text file  
-####################################################################################
-summary_mean<-function(df){
-  print(dim(df))
-  varNames<-colnames(df)
-  df_mvals<-df[,-(grep("std",varNames,ignore.case=T))]  
-  agg_mvals <-aggregate.data.frame(df_mvals[,-(1:2)], by=list(df_mvals$Subject,df_mvals$Activity),FUN=mean, na.rm=TRUE)
-   
-  nuLabs<-colnames(agg_mvals)
-  nuLabs<-paste("Average<",nuLabs,">",sep="")
-  nuLabs<-nuLabs[-(1:2)]
-  nuLabs<-c("Subject","Activity",nuLabs)
-  colnames(agg_mvals)<-nuLabs
-  print(head(agg_mvals,15)); print(dim(agg_mvals))
-  write.table(agg_mvals,"data_mean_summ.txt",row.name=F)
+#################################################################################################################
+summary_mean<-function(df,filnm="data_mean_summ.txt"){
+    #print(dim(df))
+##\ ==(2)  
+    varNames<-colnames(df)
+    df_mvals<-df[,-(grep("std",varNames,ignore.case=T))]  
+##\ ==(3)
+    agg_mvals <-aggregate.data.frame(df_mvals[,-(1:2)], by=list(df_mvals$Subject,df_mvals$Activity),FUN=mean, na.rm=TRUE)   
+##\ ==(4)
+    nuLabs<-colnames(agg_mvals)
+    nuLabs<-paste("Average<",nuLabs,">",sep="")
+    nuLabs<-nuLabs[-(1:2)]
+    nuLabs<-c("Subject","Activity",nuLabs)
+    colnames(agg_mvals)<-nuLabs
+    #print(head(agg_mvals,15)); print(dim(agg_mvals))
+##\ ==(5)
+    write.table(agg_mvals,filnm,row.name=F)
 }
